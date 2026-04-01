391641f389f5c843ef26e9899835b65d7d7e655d
pinandr101 <shevtcov_andrey109@mail.ru>
2026-04-01 11:06:29 +0300

    YearNameFix

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,6 +9,21 @@ interface HeaderProps {
 
 const now = new Date();
 
+function getEnding(n: number): string{
+  const last_two: number = n % 100;
+  const last_one: number = n % 10;
+  if (last_two >= 11 && last_two <= 14){
+    return 'лет';
+  }
+  if (last_one == 1){
+    return 'год';
+  }
+  else if (last_one >= 2 && last_one <= 4){
+    return 'года';
+  }
+  return 'лет';
+}
+
 const Header: React.FC<HeaderProps> = ({ name, foundationYear }) => {
   const dark = useTheme();
 
@@ -17,7 +32,7 @@ const Header: React.FC<HeaderProps> = ({ name, foundationYear }) => {
       <h1 className={styles.title}>Добро пожаловать в {name}!</h1>
       <p className={styles.subtitle}>
         Откройте для себя красоту и богатую историю города, основанного{" "}
-        {now.getFullYear() - foundationYear} лет назад!
+        {now.getFullYear() - foundationYear} {getEnding(now.getFullYear() - foundationYear)} назад!
       </p>
     </header>
   );
